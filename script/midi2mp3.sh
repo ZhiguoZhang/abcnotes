d625511ba2cf3d46fc18d362b418b2d42a9745c7
zhiguozhang <zhiguo.zhang@rocket-internet.de>
2014-06-03 21:58:58 +0200

    reproduce mp3 with gain=5

--- script/midi2mp3.sh
+++ script/midi2mp3.sh
@@ -10,7 +10,7 @@ if [[ -z "${FILE}" ]] ; then
 fi
 MP3FILE=${FILE%%.mid}.mp3
 WAVFILE=${FILE%%.mid}.wav
-fluidsynth -F ${WAVFILE} /usr/share/sounds/sf2/FluidR3_GM.sf2 ${FILE} &&
+fluidsynth --gain=5  -F ${WAVFILE} /usr/share/sounds/sf2/FluidR3_GM.sf2 ${FILE} &&
 lame --preset standard ${WAVFILE} ${MP3FILE} &&
 rm ${WAVFILE} &&
 mv ${MP3FILE} ../mp3/
